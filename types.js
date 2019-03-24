9e4d263707c2d389733a9e978922328217c37537
yashraikar1087 <yashraikar1087@gmail.com>
2019-03-24 13:05:09 -0600

    simple data types

--- types.js
+++ types.js
@@ -1,9 +1,12 @@
 var myString;
 var myNum;
 var myBool;
+var myVar;
 myString = "HEllo world";
 myNum = 22;
 myBool = true;
+myVar = 5;
 console.log(myString);
 console.log(myNum);
 console.log(myBool);
+console.log(myVar);
--- types.ts
+++ types.ts
@@ -1,11 +1,14 @@
 let myString: string;
 let myNum: number;
 let myBool: boolean;
+let myVar: any;
 
 myString = "HEllo world";
 myNum = 22;
 myBool = true;
+myVar = 5;
 
 console.log(myString);
 console.log(myNum);
 console.log(myBool);
+console.log(myVar);
